793364bb745f657db32d70a962a2283f92f7f333
Cloud0627 <ghasada@infoclouding.com>
2020-02-22 08:46:09 +0000

    リクエストbodyをjson化する様に

--- linebot/views.py
+++ linebot/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 def index(request):
    if request.method == 'POST':
+      # リクエスト取得
+      request_json = json.loads(request.body.decode('utf-8'))
+      # LineBot ログの出力
+      print(json.dumps(request_json,indent=2))
+
+      replyToken = request_json["events"][0]["replyToken"]
+
       return HttpResponse("This is bot api(POST).")
    else:
       return HttpResponse("This is bot api(GET).")
+
+#返信はここにデータを送る
+# https://api.line.me/v2/bot/message/reply
